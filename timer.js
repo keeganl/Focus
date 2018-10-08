438bac91d530689d5fb6260f14b64bfd8296496e
Keegan Lawley <keeganlk.7@gmail.com>
2018-10-08 15:07:10 -0500

    Started event listener implementation

--- timer.js
+++ timer.js
@@ -1,2 +1,15 @@
-var up = document.querySelector(".fa-caret-up");
+var up1 = document.querySelector("#up1");
+var up2 = document.querySelector("#up2");
+var up3 = document.querySelector("#up3");
+var up4 = document.querySelector("#up4");
 var down = document.querySelector(".fa-caret-down");
+
+function logTest() {
+  console.log("Hey");
+  
+}
+
+up1.onclick = logTest;
+up2.onclick = logTest;
+up3.onclick = logTest;
+up4.onclick = logTest;
