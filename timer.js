1ad194be7ea4a736de5006aa0501af96b45a7ddb
Keegan Lawley <keeganlk.7@gmail.com>
2018-10-08 14:44:30 -0500

    added sound and other functions

---
+++ timer.js
@@ -0,0 +1,36 @@
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10)
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+    endTimer(display);
+  }, 1000);
+  
+}
+
+function endTimer(display) {
+  display = document.querySelector("#display").value;
+  if (display <= 0) {
+    var audio = new Audio('./sounds/alarm.mp3');
+    audio.play();
+  }
+}
+
+function pomodoroStart() {
+  var pomodoro = 60 * .15,
+  display = document.querySelector("#display");
+  startTimer(pomodoro, display);
+}
+
+
+
+
